e098c9c33df2042e4ca7023f378e6f4f66e4b510
Sujan Sapkota <sujan@Sujans-MacBook-Air.local>
2025-07-13 12:40:47 +0545

    fix: fixing github ci/cd issues, commit number: 8

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -64,7 +64,7 @@ function App() {
                                 fullWidth
                               >
                                 Sign in
-                              </Button>
+                              </Button>   
                             </SignInButton>
                           </Box>
                         </Box>
